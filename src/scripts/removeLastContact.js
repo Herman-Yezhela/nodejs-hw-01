aae7e2eb3a4e3fa339b1739bcc43c99f483dd5f3
Herman Yezhela <germanegela@gmail.com>
2025-10-19 18:26:16 +0300

    0000

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,14 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if (contacts.length > 0) {
+    contacts.pop();
+    return await writeContacts(contacts);
+  } else {
+    return 'There are no contacts in database';
+  }
+};
 
 removeLastContact();
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,7 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (contacts) => {
+  const data = JSON.stringify(contacts, null, 2);
+  await fs.writeFile(PATH_DB, data, { encoding: 'utf-8' });
+};
